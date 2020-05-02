2fc88a79e451b39e3967105c6bc8f9b846e66b32
zodiac <anonymous>
2020-05-03 01:17:21 +0900

    favi

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-    // mode: 'universal',
+    mode: 'universal',
     /*
      ** Headers of the page
      */
@@ -14,7 +14,7 @@ export default {
             { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: './favicon.ico?' }
         ]
     },
     /*
