0f44260bdefa7d260911a3c7f6aeee25ae5326b6
zodiac <anonymous>
2020-05-03 07:45:55 +0900

    jidou

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div style="height: 100vh;">
+        <div id="top" style="height: 100vh;">
             <!-- <div class="cb"></div> -->
             <!-- <logo /> -->
             <div class="logo">
@@ -10,40 +10,45 @@
                 <div class="triangle4"></div>
             </div>
             <h1 class="title">
-                julienbenda.github.io
+                CYBER LAB
             </h1>
             <h2 class="subtitle">
-                My legendary Nuxt.js project
+                Nuxt.js + GitHub Pages
             </h2>
-            <div class="links">
-                <a
-                    href="https://nuxtjs.org/"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    class="button--green"
-                    >
-                    Documentation
-                </a>
-                <a
-                    href="https://github.com/nuxt/nuxt.js"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    class="button--grey"
-                    >
-                    GitHub
-                </a>
+            <!-- <div class="links"> -->
+            <!--     <a -->
+            <!--         href="https://nuxtjs.org/" -->
+            <!--         rel="noopener noreferrer" -->
+            <!--         target="_blank" -->
+            <!--         class="button&#45;&#45;green" -->
+            <!--         > -->
+            <!--         Documentation -->
+            <!--     </a> -->
+            <!--     <a -->
+            <!--         href="https://github.com/nuxt/nuxt.js" -->
+            <!--         rel="noopener noreferrer" -->
+            <!--         target="_blank" -->
+            <!--         class="button&#45;&#45;grey" -->
+            <!--         > -->
+            <!--         GitHub -->
+            <!--     </a> -->
+            <!-- </div> -->
+            <div v-if="look==2" class="scrlt">
+                <a href="#" @click="clickSmoothScroll()"><span></span>Top</a>
             </div>
-            <div id="scrl">
+            <div v-else class="scrl">
                 <a href="#" @click="clickSmoothScroll()"><span></span>Scroll</a>
             </div>
             <div id="introduction">
-                <h1 class="title" style="margin-top: 10%; color: snow;">このページについて</h1>
-                <p class="subtitle" style="margin-top: 10%; color: snow;">
+                <p class="subtitle" style="color: #35495e;">
                     このページはGitHub PagesとNuxt.jsを用いて作られています。
                 </p>
-                <p class="subtitle" style="margin-top: 10%; color: snow;">
+                <p class="subtitle" style="margin-top: 10%; color: #35495e;">
                     このように公開する方法を紹介する記事を上げていきたいと思います。
                 </p>
+            </div>
+            <div id="blogs">
+                <h2 class="subtitle" style="color: #35495e;">記事一覧</h2>
             </div>
         </div>
     </div>
@@ -57,11 +62,18 @@ import smoothScroll from 'vue-smoothscroll';
 Vue.use(smoothScroll);
 
 export default {
+    data: function() {
+        return {
+            look: 0,
+            dom_ids: ['#introduction', '#blogs', '#top']
+        }
+    },
     methods: {
         clickSmoothScroll () {
             event.preventDefault();
+            if (this.look > this.dom_ids.length - 1) this.look = 0;
             this.$SmoothScroll(
-                document.querySelector('#introduction'),
+                document.querySelector(this.dom_ids[this.look++]),
                 400,
                 null,
                 null,
@@ -90,13 +102,22 @@ export default {
         --logo-scale: 0;
     }
 }
+#blogs {
+    position: absolute;
+    left: 0;
+    top: 200vh;
+    height: 100vh;
+    width: 100vw;
+    padding: 5vh 50px 0px 50px;
+}
 #introduction {
     position: absolute;
     left: 0;
     top: 100vh;
-    background: coral;
+    background: #41B883;
     height: 100vh;
     width: 100vw;
+    padding: 25vh 50px 50px 50px;
 }
 /* .cb { */
 /*     border-left: 1px solid black; */
@@ -107,17 +128,18 @@ export default {
 /*     position: fixed; */
 /*     z-index: 362187; */
 /* } */
-#scrl {
+.scrl, .scrlt{
     position: fixed;
     top: 90%;
     left: 50%;
-    width: 100px;
-    margin: 0 0 0 -50px;
+    width: 200px;
+    height: 50px;
+    margin: 0 0 0 -100px;
     text-align: center;
     z-index: 10;
 }
 
-#scrl a {
+.scrl a, .scrlt a {
     padding: 15px;
     color: #35495e;
     text-decoration: none;
@@ -125,7 +147,7 @@ export default {
 /* #scrl a:active, #scrl a:hover{ */
 /*     background: rgba(0,0,0,0); */
 /* } */
-#scrl a span {
+.scrl a span {
     position: absolute;
     left: 50%;
     width: 24px;
@@ -139,6 +161,47 @@ export default {
     -webkit-animation: sdb 1.5s infinite;
     animation: sdb 1.5s infinite;
     box-sizing: border-box;
+}
+.scrlt a span {
+    position: absolute;
+    left: 50%;
+    width: 24px;
+    height: 24px;
+    margin-left: -12px;
+    margin-top: -70px;
+    border-right: 1px solid #35495e;
+    border-top: 1px solid #35495e;
+    -webkit-transform: rotateZ(-45deg);
+    transform: rotateZ(-45deg);
+    -webkit-animation: sdbt 1.5s infinite;
+    animation: sdbt 1.5s infinite;
+    box-sizing: border-box;
+}
+@-webkit-keyframes sdbt {
+    0% {
+        -webkit-transform: rotateY(0) rotateZ(-45deg) translate(-20px, 20px);
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        -webkit-transform: rotateY(720deg) rotateZ(-45deg) translate(0, 0);
+        opacity: 0;
+    }
+}
+@keyframes sdbt {
+    0% {
+        transform: rotateY(0) rotateZ(-45deg) translate(-20px, 20px);
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        transform: rotateY(720deg) rotateZ(-45deg) translate(0, 0);
+        opacity: 0;
+    }
 }
 @-webkit-keyframes sdb {
     0% {
